2b3ee0a7ee7cb7d2cb02df02962311ec219b3cd0
Adriano Araujo Felisberto <adrianofelisberto1990@gmail.com>
2020-04-10 21:27:39 -0300

    style: alinha imagem do componente

--- src/app/shared/shared-components/components/imagem-octocat/imagem-octocat.component.ts
+++ src/app/shared/shared-components/components/imagem-octocat/imagem-octocat.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-imagem-octocat',
   templateUrl: './imagem-octocat.component.html',
   styleUrls: ['./imagem-octocat.component.scss']
 })
-export class ImagemOctocatComponent {}
+export class ImagemOctocatComponent {
+
+  @Input() tamanho = 60;
+
+  @HostBinding('attr.style')
+  public get valorString(): any {
+    return this.sanitizer.bypassSecurityTrustStyle(`--porcentagem-img: ${this.tamanho}%`);
+  }
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+
+}
