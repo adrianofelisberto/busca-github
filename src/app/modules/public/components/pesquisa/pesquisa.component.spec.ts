e3eacb68964908f1bb8379012a582e7acd52a88c
Adriano Araujo Felisberto <adrianofelisberto1990@gmail.com>
2020-04-11 13:07:52 -0300

    test: componente de pesquisa

--- src/app/modules/public/components/pesquisa/pesquisa.component.spec.ts
+++ src/app/modules/public/components/pesquisa/pesquisa.component.spec.ts
@@ -1,9 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { PesquisaComponent } from './pesquisa.component';
 import { GithubService } from '../../services/github.service';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+import { USUARIO } from '../../services/usuario.mock';
+import { UsuarioGitHub } from '../../models/usuario-github.model';
 
 describe('PesquisaComponent', () => {
   let component: PesquisaComponent;
@@ -32,4 +35,33 @@ describe('PesquisaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve limpar campo de usuário', () => {
+    component.username.setValue('usuarioMocado');
+    expect(component.username.value).toBeDefined();
+    component.limparPesquisa(true);
+    expect(component.username.value).toBeNull();
+  });
+
+  it('não deve limpar campo de usuário', () => {
+    component.username.setValue('usuarioMocado');
+    component.limparPesquisa(false);
+    expect(component.username.value).toBeDefined();
+  });
+
+  it('deve buscar o usuário pelo login', async(inject([GithubService, HttpTestingController], (service, backend) => {
+    service.buscarUsuario('adrianofelisberto').subscribe((usuario: UsuarioGitHub) => {
+      expect(usuario).toBeDefined();
+    });
+    const request: any = backend.expectOne(`${environment.apiUrl}/users/adrianofelisberto`);
+    request.flush(USUARIO);
+
+    component.pesquisarUsuario(true);
+  })));
+
+  it('não deve buscar o usuário pelo login', async(inject([GithubService], (service) => {
+    const serviceSpy = spyOn(service, 'buscarUsuario');
+    component.pesquisarUsuario(false);
+    expect(serviceSpy).toHaveBeenCalledTimes(0);
+  })));
 });
