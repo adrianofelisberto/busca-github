dad31f0accd1a2c44efc9561e4fe75b42ce27dce
Adriano Araujo Felisberto <adrianofelisberto1990@gmail.com>
2020-04-11 14:33:05 -0300

    feat: ajuste nomenclatura das açoes

--- src/app/modules/core/consts/action.const.ts
+++ src/app/modules/core/consts/action.const.ts
@@ -2,21 +2,16 @@ import { Action } from '@ngrx/store';
 
 import { ActionTypes } from '../enuns/action-types.enum';
 
-const ADICIONAR = (objeto: any) => {
+export const Adicionar = (objeto: any) => {
   return {
     type: ActionTypes.ADICIONAR,
     payload: objeto
   } as Action;
 };
 
-const LIMPAR = () => {
+export const Limpar = () => {
   return {
     type: ActionTypes.LIMPAR,
     payload: null
   } as Action;
-};
-
-export const ACTION = {
-  ADICIONAR,
-  LIMPAR
 };
