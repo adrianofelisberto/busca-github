772450e73277e8b390354c7bda59d8ed928d0f80
Adriano Araujo Felisberto <adrianofelisberto1990@gmail.com>
2020-04-12 06:31:58 -0300

    test: ajusta retorno de método

--- src/app/modules/core/reducers/usuario-reducer.spec.ts
+++ src/app/modules/core/reducers/usuario-reducer.spec.ts
@@ -3,7 +3,6 @@ import { usuarioReducer } from './usuario-reducer';
 import { USUARIO } from '../../../shared/consts/usuario.mock';
 import { UsuarioGitHub } from 'src/app/shared/shared-models/models/usuario-github.model';
 import { ActionModel } from '../models/action.model';
-import { ActionTypes } from '../enuns/action-types.enum';
 
 describe('usuario-reducer', () => {
 
@@ -15,7 +14,7 @@ describe('usuario-reducer', () => {
 
 
   it('deve iniciar um usuário', () => {
-    const actionModel: ActionModel = Adicionar(usuarioMock);
+    const actionModel: ActionModel = Adicionar(usuarioMock) as ActionModel;
     const novoEstado: UsuarioGitHub = usuarioReducer(null, actionModel);
     expect(novoEstado).toEqual(usuarioMock);
   });
