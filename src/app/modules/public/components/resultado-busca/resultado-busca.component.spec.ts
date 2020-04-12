a00cf61bf29d47d58f64942e534e535c90d45651
Adriano Araujo Felisberto <adrianofelisberto1990@gmail.com>
2020-04-12 00:11:28 -0300

    test: ajuste nos testes unitários

--- src/app/modules/public/components/resultado-busca/resultado-busca.component.spec.ts
+++ src/app/modules/public/components/resultado-busca/resultado-busca.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultadoBuscaComponent } from './resultado-busca.component';
+import { CoreModule } from 'src/app/modules/core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PesquisaComponent } from '../pesquisa/pesquisa.component';
 
 describe('ResultadoBuscaComponent', () => {
   let component: ResultadoBuscaComponent;
@@ -8,7 +13,18 @@ describe('ResultadoBuscaComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResultadoBuscaComponent ]
+      declarations: [
+        ResultadoBuscaComponent,
+        PesquisaComponent
+      ],
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule,
+        CoreModule,
+        RouterTestingModule.withRoutes(
+          [{path: '', component: PesquisaComponent}]
+        )
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +38,9 @@ describe('ResultadoBuscaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve voltar para tela de pesquisa', () => {
+    component.voltarPesquisa();
+  });
+
 });
