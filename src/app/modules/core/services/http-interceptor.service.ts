689e0b05572e7d9d67a966e73d2f20526adc7fed
Adriano Felisberto <adriano.felisberto@ctis.com.br>
2020-04-13 18:17:08 -0300

    chore: comentario da licença

--- src/app/modules/core/services/http-interceptor.service.ts
+++ src/app/modules/core/services/http-interceptor.service.ts
@@ -1,9 +1,29 @@
+/***************************************************************************************************
+ * This program search an user from GitHub
+ * Copyright (C) 2020 Adriano Araújo Felisberto
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpErrorResponse, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+
 import { catchError, map } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
-import { StatusHttp } from 'src/app/shared/enuns/status-http.enum';
+
 import { MensagemService } from 'src/app/shared/shared-service/services/mensagem.service';
+import { StatusHttp } from 'src/app/shared/enuns/status-http.enum';
 import { AppMensagem } from 'src/app/shared/consts/app-mensagem.const';
 
 @Injectable()
@@ -14,7 +34,6 @@ export class HttpInterceptorService implements HttpInterceptor {
   ) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('interceptor');
     return next.handle(request).pipe(
       map((event) => {
         return event;
