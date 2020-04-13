6b710c0a13af641661dca4b3c176745dd8c7d3b3
Adriano Felisberto <adriano.felisberto@ctis.com.br>
2020-04-13 18:16:14 -0300

    feat: cria componente de erro do servidor

---
+++ src/app/modules/public/modules/erro/components/erro-servidor/erro-servidor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-erro-servidor',
+  templateUrl: './erro-servidor.component.html',
+  styleUrls: ['./erro-servidor.component.scss']
+})
+export class ErroServidorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
